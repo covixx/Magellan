3d869585afa8b4a9b33c7a9ab349b7d7027fb069
covixx <notcovix@gmail.com>
2024-09-26 16:53:53 +0530

    empty taskform

--- features/accounts/components/task-form.tsx
+++ features/accounts/components/task-form.tsx
@@ -8,73 +8,8 @@ import { Input } from "@/components/ui/input";
 import {Form, FormField, FormDescription, FormControl, FormLabel, FormMessage, FormItem} from "@/components/ui/form";
 import { FormValue } from "hono/types";
 
-const formschema = insertaccountschema.pick({
-    name: true,
-});
 
-type formvalues = z.input<typeof formschema>;
 
-type Props = {
-    id?: string,
-    defaultvalues?: formvalues;
-    onsubmit: (values: formvalues) => void;
-    ondelete?: () => void;
-    disabled?: boolean;
-};
-
-export const TaskForm = ({
-    id, 
-    defaultvalues, 
-    onsubmit, 
-    ondelete,
-    disabled,
-}:Props) => {
-    const form = useForm<formvalues>({
-        resolver: zodResolver(formschema),
-        defaultValues: defaultvalues, 
-    });
-    const handlesubmit = (values: formvalues) => {
-        onsubmit(values);
-    };
-    const handledelete = () => {
-        ondelete?.();
-    };
-    return (
-        <Form{...form}>
-            <form onSubmit={form.handleSubmit(handlesubmit)}className="space-y-4 pt-4">
-                <FormField
-                name="name"
-                control={form.control}
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>
-                            Name
-                        </FormLabel>
-                        <FormControl>
-                            <Input
-                            disabled = {disabled}
-                            placeholder="e.g. Work, Gym, Studying"
-                            {...field}
-                            />
-                        </FormControl>
-                    </FormItem>
-                )}/>
-                <Button className="w-full" disabled={disabled}>
-                    {id? "Save changes": "Create Task"}
-                </Button>
-                    {!!id && (
-                        <Button
-                        type = "button"
-                        disabled = {disabled}
-                        onClick={handledelete}
-                        className="w-full"
-                        variant="outline"
-                    >
-                        <Trash className="size-4 mr-2"/>
-                        Delete task
-                    </Button>
-                )}
-            </form>
-        </Form>
-    )
+export const TaskForm = {
+    
 }
